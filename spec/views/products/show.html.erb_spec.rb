5a0898c4dce67571ed4611f5b37f463aeb8c4680
Nick Lewis <nickeblewsi@gmail.com>
2026-09-23 08:42:31 +0100

    version 0.1

---
+++ spec/views/products/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "products/show", type: :view do
+  before(:each) do
+    assign(:product, Product.create!(
+      title: "Title",
+      description: "MyText",
+      image: nil,
+      price: "9.99"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Title/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/9.99/)
+  end
+end
